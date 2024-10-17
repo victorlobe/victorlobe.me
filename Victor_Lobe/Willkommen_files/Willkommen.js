b5f5125e8b279cbfd173d7173fef13a96a2ec322
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2024-10-17 20:35:56 +0200

    .

--- Victor_Lobe/Willkommen_files/Willkommen.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.2 local-build-20241016
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Willkommen_files/WillkommenMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
